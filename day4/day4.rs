519813277c4041871833a5dde99487c01e89e806
harryjulian <harry.julian@peak.ai>
2022-12-05 00:30:58 +0000

    day4 part2

--- day4/day4.rs
+++ day4/day4.rs
@@ -1,4 +1,6 @@
 use std::fs;
+use std::collections::HashSet;
+use std::convert::TryInto;
 
 fn main() {
 
@@ -8,8 +10,12 @@ fn main() {
   let pairs: Vec<&str> = binding.split('\n').collect();
   
   // Get first solution
-  let solution_1 = solve_1(pairs);
-  println!("The solution to part 1 is: {solution_1:?}")
+  let solution_1 = solve_1(pairs.clone());
+  println!("The solution to part 1 is: {solution_1:?}");
+
+  // Get second solution
+  let solution_2 = solve_2(pairs.clone());
+  println!("The solution to part 2 is: {solution_2:?}");
 
 }
 
@@ -32,6 +38,25 @@ fn solve_1(pairs: Vec<&str>) -> i32 {
       solution +=1;
     }
   
+  }
+  solution
+}
+
+fn solve_2(pairs: Vec<&str>) -> i32 {
+
+  // In how many assignment pairs do the ranges overlap?
+  let mut solution: i32 = 0;
+  let iter = pairs;
+
+  for pair in iter {
+    let pair_vec: Vec<i32> = pair.split(",").map(|x| x.parse().unwrap()).collect();
+    let first_range = (pair_vec[0]..pair_vec[1]).collect::<HashSet<_>>();
+    let second_range = (pair_vec[2]..pair_vec[3]).collect::<HashSet<_>>();
+    let intersect = first_range.intersection(&second_range).collect::<Vec<_>>();
+    let overlap: i32 = intersect.len().try_into().unwrap();
+    if overlap >= 1 {
+      solution += 1;
+    } 
   }
   solution
 }
